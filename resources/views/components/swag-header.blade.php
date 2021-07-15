d69eb68924a1631f41ef0d9c27d935c57f462795
Victor Rosu <victor.rosu@ringhel.com>
2021-07-15 18:16:14 +0300

    Swag header update for frontend

--- resources/views/components/swag-header.blade.php
+++ resources/views/components/swag-header.blade.php
@@ -23,12 +23,7 @@
                 <a href="#" data-toggle="search" class="nav-link nav-link-lg d-md-none navsearch">
                     <svg class="header-icon search-icon" x="1008" y="1248" viewBox="0 0 24 24" height="100%" width="100%" preserveAspectRatio="xMidYMid meet" focusable="false"><path d="M0 0h24v24H0V0z" fill="none"></path><path d="M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"></path></svg>
                 </a>
-                <div class="dropdown profile-dropdown">
-                    <a href="#" class="nav-link pr-0 leading-none" data-toggle="dropdown" aria-expanded="false"><img src="{{ url('/th/assets/images/users/account.png') }}" alt="img" class="avatar-md brround"></a>
-                    <div class="dropdown-menu dropdown-menu-right dropdown-menu-arrow animated">
-                        <x-user-menu section="front" />
-                    </div>
-                </div>
+                <x-user-menu section="front" />
             </div>
         </div>
     </div>
