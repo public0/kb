d82a621e9afa0fbda0e52965aa34208cebdb0669
Victor Rosu <victor.rosu@ringhel.com>
2021-07-20 18:06:06 +0300

    Categories fix

--- app/Http/Controllers/Admin/CategoriesController.php
+++ app/Http/Controllers/Admin/CategoriesController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Models\Article;
 use App\Models\Category;
 use App\Models\Language;
+use App\MyClasses\UtileClass;
 use Exception;
 use Illuminate\Http\Request;
 
