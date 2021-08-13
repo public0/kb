8481c1163d875f821dd032d2cdce9ef8124bff81
Victor Rosu <victor.rosu@ringhel.com>
2021-08-13 16:56:04 +0300

    User account email signature update

--- resources/views/emails/user-account.blade.php
+++ resources/views/emails/user-account.blade.php
@@ -11,5 +11,12 @@
     </p>
     <p><i>If the link does not work please copy it in the browser.</i></p>
     <p>&nbsp;</p>
-    <p>Thank you,<br />{{ config('app.name') }} Team</p>
+    <p>Thank you,</p>
+    <span>
+        <img src="{{ url('/th/assets/images/brand/logo.png') }}" alt="{{ config('app.name') }}" height="40" />
+        <img src="{{ url('/th/assets/images/brand/logo_procesio.png') }}" alt="{{ config('app.name') }}" height="40" />
+    </span>
+    <p>14th Geniului Boulevard, 6th District, Bucharest, Romania - 060117</p>
+    <p><b style="color:#900">Support:</b> +40 771 111 222</p>
+    <p><a href="http://www.ringhel.com" target="_blank" style="color:#009">www.ringhel.com</a> | <a href="http://www.procesio.com" target="_blank" style="color:#009">www.procesio.com</a></p>
 @endsection
