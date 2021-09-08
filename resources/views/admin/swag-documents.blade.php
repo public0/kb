fd918486ee3701650c41d1d9c7692c5fb1035e38
Victor Rosu <victor.rosu@ringhel.com>
2021-09-08 10:50:54 +0300

    Swag admin module update

--- resources/views/admin/swag-documents.blade.php
+++ resources/views/admin/swag-documents.blade.php
@@ -37,7 +37,7 @@
                 <div class="card-body">
                     @if(!empty($documents))
                     <div class="table-responsive">
-                        <table class="table table-bordered text-nowrap" id="example1">
+                        <table class="table table-bordered" id="example1">
                             <thead>
                             <tr>
                                 <th class="wd-15p border-bottom-0">Name</th>
